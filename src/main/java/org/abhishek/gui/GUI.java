d4bb25ad2ebfbcc99c12538b924c3c6492353253
Abhishek Choure <abhishekchoure222@gmail.com>
2020-08-15 00:14:58 +0530

    added a check to ensure database connects

--- src/main/java/org/abhishek/gui/GUI.java
+++ src/main/java/org/abhishek/gui/GUI.java
@@ -1,7 +1,10 @@
 package org.abhishek.gui;
 
-import javax.swing.JFrame;
+import org.abhishek.credentials.Configuration;
+
+import javax.swing.*;
 import java.awt.*;
+import java.sql.Connection;
 
 public class GUI {
 
@@ -14,6 +17,7 @@ public class GUI {
         final static String title = "Budgety";
         final static int HEIGHT = 800;
         final static int WIDTH = HEIGHT/12 * 9;
+        private Connection connect = null;
 
         public MainGUI() {
             mainFrame = new JFrame(title);
@@ -22,6 +26,12 @@ public class GUI {
             mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             new IndexFrame(mainFrame);
             mainFrame.setVisible(true);
+            connect = Configuration.connectToDatabase();
+            if(connect == null) {
+                String msg = "\nMake sure you have Postgresql 10 or higher installed OR if you do have then please run the budgety_database.sql in your psql shell and then try again";
+                JOptionPane.showMessageDialog(mainFrame, "Couldn't connect to the database :( " + msg,"Database Connection Error",JOptionPane.ERROR_MESSAGE);
+                System.exit(1);
+            }
         }
     }
 }
